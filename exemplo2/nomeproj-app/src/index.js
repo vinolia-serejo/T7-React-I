dff25091de0b04aff417c9a14aa66090c4be3731
vinolia-serejo <vinolia.serejo@gmail.com>
2019-06-11 16:46:32 -0300

    usando componentes funcionais e classes em react

--- exemplo2/nomeproj-app/src/index.js
+++ exemplo2/nomeproj-app/src/index.js
@@ -1,32 +1,120 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import './App.css';
 // import App from './App';arquivo que deletei
 import * as serviceWorker from './serviceWorker';
 
-let contadorNum = 780;
 
-const nome = {
-    primeiroNome: "Vinolia",
-    sobreNome:"Serejo"
+
+// const nome = {
+//     primeiroNome: "Vinolia",
+//     sobreNome:"Serejo"
+// }
+
+// function nomeCompleto(nome){
+//     return nome.primeiroNome + '' + nome.sobreNome;
+// }
+
+// let contadorNum = 1;
+//     function verificaNum(contador){
+//         if(contador === 0){
+//             return <p>Contador não iniciado</p>
+//         }else{
+//             return <p>Contador iniciado</p>
+//         }
+// }
+
+// setInterval(function(){
+//     //contadorNum++
+//     const template = 
+//                 <div className="contador">
+//                     <h1>Count: {contadorNum}</h1>
+//                     <div>
+//                         <button>+1</button>
+//                         <button>-1</button>
+//                         <button>Reset</button>
+//                     </div> 
+//                 <div>
+//                     <p>{verificaNum(contadorNum)}</p>
+//                 </div>
+//                 </div>
+
+//         ReactDOM.render(template, document.getElementById('root'));
+// },1000);
+
+
+
+// // If you want your app to work offline and load faster, you can change
+// // unregister() to register() below. Note this comes with some pitfalls.
+// // Learn more about service workers: https://bit.ly/CRA-PWA
+// serviceWorker.unregister();
+
+
+
+
+// function nomeCompleto(nome){
+//     return nome.primeiroNome + '' + nome.sobreNome;
+// }
+
+// let contadorNum = 1;
+//     function verificaNum(contador){
+//         if(contador === 0){
+//             return <p>Contador não iniciado</p>
+//         }else{
+//             return <p>Contador iniciado</p>
+//         }
+// }
+
+
+// exibindo cards na tela
+class Comentario extends React.Component {
+    render() {
+        return(
+            
+            <div className="comentario">
+                
+                <img className="comentario__perfil" src={this.props.imagem}></img>
+                <div>
+                    <h2 className="comentario__nome">{this.props.nome}</h2>
+                    <h3 className="comentario__subtitulo">{this.props.subtitulo}</h3>
+                    <hr/>
+                    <p>{this.props.comentario}
+                    Mussum Ipsum, cacilds vidis litro abertis. Copo furadis é disculpa de bebadis, arcu quam euismod magna. Admodum accumsan disputationi eu sit. Vide electram sadipscing et per. Não sou faixa preta cumpadi, sou preto inteiris, inteiris. Todo mundo vê os porris que eu tomo, mas ninguém vê os tombis que eu levo!
+                    </p> 
+                </div>
+            </div>
+        )
+    }
 }
 
-function nomeCompleto(nome){
-    return nome.primeiroNome + '' + nome.sobreNome;
-}
-const template = 
-                <div className="contador">
-                    <h1>Count: {contadorNum}</h1>
-                    <h1>Oi {nome.primeiroNome}</h1>
-                    <h1>Olá {nomeCompleto(nome)}</h1>
-                    <button>+1</button>
-                    <button>-1</button>
-                    <button>reset</button>
 
-                 </div>
+class App extends React.Component {
+    render() {
+        return(
+            <div>
+                <Comentario
+                imagem="https://t2.ea.ltmcdn.com/pt/images/0/2/5/img_remedio_caseiro_para_acalmar_gato_22520_600.jpg"
+                nome="Rafaela"
+                subtitulo="perfeita"
+                comentario="lorem ipsum dolor sit amet"
+                />
+                <Comentario
+                imagem="https://t2.ea.ltmcdn.com/pt/images/0/2/5/img_remedio_caseiro_para_acalmar_gato_22520_600.jpg"
+                nome="Vivy"
+                subtitulo="amorzinho"
+                comentario="lorem ipsum dolor sit amet"/>
+            </div>
+        
+        )
+    } 
+}    
+    ReactDOM.render(<App/>, document.getElementById('root'));
+
+  
 
 
-ReactDOM.render(template, document.getElementById('root'));
+
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
